fbb4e7c6c2b433ee461e785a9240fc6bd48ac978
aminbabu12 <badrulaminbabu@gmail.com>
2025-12-12 15:37:46 +0600

    Project set up done

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qyacehm.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('StyleDcor Server is Running')
+});
+
+async function run() {
+  try {
+    await client.connect();
+    
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`StyleDcor Server is Running on port ${port}`)
+});
